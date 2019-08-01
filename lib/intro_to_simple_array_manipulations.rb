6ba3f12204c50855f9f5d7ea6d68bbbe738c97fe
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-01 00:02:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,10 @@ def shift_with_args(array)
   array.shift(2)  
 end
 
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
 def using_insert(array, element)
   array.insert(4, element)
 end
